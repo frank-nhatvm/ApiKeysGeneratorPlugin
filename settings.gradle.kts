61010a01b30fd7631ae91e01827ffebb93fb92ec
frank vu <nhat.thtb@gmail.com>
2025-06-30 11:57:33 +0700

    setup to publish

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,14 @@
+pluginManagement {
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "ApiKeyGenerators"
 include("app")
+include(":api-key-generator-plugin")
